a2b56ed58b15be386dbe5fd4112bbdc86423c906
vincenttian <vincenttian16@gmail.com>
2014-06-30 16:39:37 -0700

    back to node.js app

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn website.wsgi
+web: node server.js
--- website/app1/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from website.application_portal.models import Applicant
-
-admin.site.register(Applicant)
